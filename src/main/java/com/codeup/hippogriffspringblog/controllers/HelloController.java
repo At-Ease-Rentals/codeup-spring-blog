4216f718e57cb8698783cd256715dab25d02cca3
Anthony Gantt <anthony@pcsatease.com>
2023-12-11 11:45:44 -0600

    MVC exercise and controller classes.

--- src/main/java/com/codeup/hippogriffspringblog/controllers/HelloController.java
+++ src/main/java/com/codeup/hippogriffspringblog/controllers/HelloController.java
@@ -1,8 +1,41 @@
 package com.codeup.hippogriffspringblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
+    private int counter = 0;
 
+    @GetMapping("/counter/{counterInit}")
+    @ResponseBody
+    public String initCounter(@PathVariable int counterInit){
+        counter = counterInit;
+        return "counter reset to " + counter;
+    }
+
+    @GetMapping("/increment")
+    @ResponseBody
+    public String incrementCounter() {
+         counter++;
+        return "counter is now:" + counter;
+    }
+
+
+//    @RequestMapping(path = "/hello", method = RequestMethod.GET)
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello from Spring!";
+//
+//        @GetMapping(path = "/hello/{personName}")
+//        @ResponseBody
+//        public String hello (@PathVariable String  personName){
+//            return "Hello " + personName;
+//        }
+//
+//        @GetMapping(path = "/hello-msg/{personName}", produces = "applications/json")
+//        @ResponseBody
+//        public String helloMessage () {
+//            ){
+//                return String.format("Hello from JSON! %s %s", personName, LocalDate.now());
 }
